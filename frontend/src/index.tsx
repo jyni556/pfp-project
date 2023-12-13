3ba311d22724ac9599fc9b0bafaad4e4922b2251
jyni556 <devdesi10@gmail.com>
2023-12-13 18:45:48 +0900

    MetaMask SDK Setting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MetaMaskProvider
+      sdkOptions={{
+        dappMetadata: {
+          name: "PFP Project p",
+          url: window.location.host,
+        },
+      }}
+    >
+      <App />
+    </MetaMaskProvider>
   </React.StrictMode>
 );
 
